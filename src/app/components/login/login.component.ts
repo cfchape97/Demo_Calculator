1d8b0e66bd173db1737c1b5a507b02dca596f07c
cfchape97 <chasechape@gmail.com>
2024-04-05 19:52:26 -0400

    Refactor button into button component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { InputComponent } from '../shared/input/input.component';
+import { ButtonComponent } from '../shared/button/button.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [InputComponent],
+  imports: [InputComponent, ButtonComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
