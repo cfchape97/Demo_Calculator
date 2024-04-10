1b03495fcadb010b6e69989be14a8ff50c72ad68
cfchape97 <chasechape@gmail.com>
2024-04-10 18:30:56 -0400

    Refactor calculator buttons into custom compoenent and add styling

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CalcButtonComponent } from '../shared/calc-button/calc-button.component';
+import { CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-calculator',
   standalone: true,
-  imports: [],
+  imports: [CalcButtonComponent, CommonModule],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.scss'
 })
 export class CalculatorComponent {
+  buttons: string[][] = [];
+
+  constructor() {
+    this.populateButtons();
+  }
+
+  populateButtons() {
+    this.buttons = [['1','2','3','+','('], ['4','5','6','-',')'], ['7','8','9','*','.']];
+  }
 
 }
---
+++ src/app/components/shared/calc-button/calc-button.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit, input } from '@angular/core';
+
+@Component({
+  selector: 'app-calc-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './calc-button.component.html',
+  styleUrl: './calc-button.component.scss'
+})
+export class CalcButtonComponent implements OnInit {
+  @Input() text: string = '';
+  @Input() width: number = 1;
+  @Input() height: number = 1;
+  buttonWidth = 60;
+  buttonHeight = 60;
+
+  ngOnInit() {
+    this.buttonWidth = 60 * this.width + 16 * (this.width - 1);
+    this.buttonHeight = 60 * this.height + 16 * (this.height - 1);
+  }
+}
