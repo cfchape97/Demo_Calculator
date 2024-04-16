339bec587001e5935d099598e0cb779524ae330d
cfchape97 <chasechape@gmail.com>
2024-04-16 19:36:59 -0400

    add activated route to testing for login component

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,17 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
+
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      imports: [LoginComponent], providers: [{provide: ActivatedRoute, useValue: {}}]
     })
       .compileComponents();
-
+      
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,12 @@
 import { Component} from '@angular/core';
 import { InputComponent } from '../shared/input/input.component';
 import { ButtonComponent } from '../shared/button/button.component';
-import { LinkComponent } from '../shared/link/link.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [InputComponent, ButtonComponent, LinkComponent, RouterModule],
+  imports: [InputComponent, ButtonComponent, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
