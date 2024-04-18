efe78a4ac4115cbe7a1794be425690caa404c098
cfchape97 <chasechape@gmail.com>
2024-04-17 20:08:38 -0400

    add starting code for reactive forms

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { InputComponent } from '../shared/input/input.component';
 import { CardComponent } from '../shared/card/card.component';
 import { ButtonComponent } from '../shared/button/button.component';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-contact',
   standalone: true,
@@ -10,5 +11,16 @@ import { ButtonComponent } from '../shared/button/button.component';
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  contactForm: FormGroup;
 
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = fb.group({
+      firstName: new FormControl(), 
+      lastName: new FormControl(), 
+      email: new FormControl(), 
+      message: new FormControl(), 
+    });
+  }
+  
 }
